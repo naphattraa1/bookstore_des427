b346e67e9a41f5cb6903553a56af14bf3f8bc608
Une07 <une04958@gmail.com>
2025-11-13 14:50:30 +0700

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,7 +1,17 @@
 // components/firebase/firebase.js
 
-// Placeholder file for Firebase configuration.
-// If your teacher later requires real Firebase auth,
-// you can put initializeApp(...) etc. here.
+const firebaseConfig = {
+  apiKey: "AIzaSyCDm_zmY3ILrtfAWHozT1msq-u1lVNISYs",
+  authDomain: "book-store-efdd9.firebaseapp.com",
+  projectId: "book-store-efdd9",
+  storageBucket: "book-store-efdd9.firebasestorage.app",
+  messagingSenderId: "93304152441",
+  appId: "1:93304152441:web:b1563a7f61f1e449738fa8",
+  measurementId: "G-3L556SP9B3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export {};
