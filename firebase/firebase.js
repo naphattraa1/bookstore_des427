9e149dc2e70e9370eed54511e3f240205ff2daac
Une07 <une04958@gmail.com>
2025-11-15 10:10:17 +0700

    Updated firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
-// เอา config ที่น้องมีมาใส่ตรงนี้
+admin.auth().setCustomUserClaims(uid, { admin: true });
 const firebaseConfig = {
   apiKey: "AIzaSyCDm_zmY3ILrtfAWHozT1msq-u1lVNISYs",
   authDomain: "book-store-efdd9.firebaseapp.com",
@@ -13,11 +13,8 @@ const firebaseConfig = {
   measurementId: "G-3L556SP9B3",
 };
 
-// ให้แน่ใจว่า app ถูก initialize แค่ครั้งเดียว
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
-// สร้าง instance ของ Realtime Database
 const database = getDatabase(app);
 
-// export ออกไปให้ไฟล์อื่นใช้
 export { app, database };
